859f0435bac261475491d5ec2167f13518440e8a
Obinna Onyimadu <obinna240@yahoo.co.uk>
2021-05-26 14:30:59 +0100

    implementing controllers

--- user.cmd.api/src/main/java/com/org/obtech/user/cmd/api/commands/UpdateUserCommand.java
+++ user.cmd.api/src/main/java/com/org/obtech/user/cmd/api/commands/UpdateUserCommand.java
@@ -5,10 +5,15 @@ import lombok.Builder;
 import lombok.Data;
 import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
 @Data
 @Builder
 public class UpdateUserCommand {
     @TargetAggregateIdentifier
     private String id;
+    @Valid
+    @NotNull(message = "user cannot be null or blank")
     private User user;
 }
---
+++ user.cmd.api/src/main/java/com/org/obtech/user/cmd/api/controller/UpdateUserController.java
@@ -0,0 +1,41 @@
+package com.org.obtech.user.cmd.api.controller;
+
+import com.org.obtech.user.cmd.api.commands.RegisterUserCommand;
+import com.org.obtech.user.cmd.api.commands.UpdateUserCommand;
+import com.org.obtech.user.cmd.api.dto.BaseResponse;
+import com.org.obtech.user.cmd.api.dto.RegisterUserResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
+
+@Slf4j
+@RestController
+@RequestMapping(path = "/api/v1/updateuser")
+public class UpdateUserController {
+
+    private final CommandGateway commandGateway;
+
+    @Autowired
+    public UpdateUserController(CommandGateway commandGateway){
+        this.commandGateway = commandGateway;
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<BaseResponse> updateUser(@Valid @RequestBody UpdateUserCommand updateUserCommand, @PathVariable("id") String id) {
+        try {
+            //set the id in the command
+            updateUserCommand.setId(id);
+            commandGateway.sendAndWait(updateUserCommand);
+
+            return new ResponseEntity<>(new BaseResponse("Successful update"), HttpStatus.OK);
+        } catch (Exception exception) {
+            var safeErrorMessage = "Error processing user update for "+ id;
+            log.error(exception.getMessage());
+            return new ResponseEntity<>(new RegisterUserResponse(id, safeErrorMessage), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
